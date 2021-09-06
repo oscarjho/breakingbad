614bcbf64ff9b84aca65d6ce3d2020ebfa222589
Oscar <oscarjavierho@gmail.com>
2021-09-06 10:51:37 -0430

    Agregando styled

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import styled from '@emotion/styled';
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`;
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
 
 function App() {
   return (
-    <h1> Breaking bad </h1>
+    <Contenedor> 
+      <Boton>
+        Obtener Frase
+      </Boton>
+    </Contenedor>
   );
 }
 
